70d92d5f29b3281478b81d4de43656877b8f7b2b
llunareine <llunareine@gmail.com>
2022-09-22 02:59:55 +0600

    initialize project

---
+++ lab1.py
@@ -0,0 +1,45 @@
+import math
+# x = input()
+# for i in range(1, int(x) + 1):
+#     print(" Number is : " + str(i) + " and cube of the " + str(i) + " is " + str(int(math.pow(i,3))))
+
+# mylist = [input() for i in range(0,5)]
+# mylist.reverse()
+# for i in mylist:
+#     print(i)
+
+# f = 1
+# x = input()
+# for i in range(1, int(x) + 1):
+#     f = f * i
+# print(f)
+
+# f = 0
+# x = input()
+# for i in range(1, int(x) + 1):
+#     f = f + i
+# print(f)
+
+# x = input()
+# original = x
+# mylist = []
+# for i in range(len(str(x))):
+#     mylist.append(str(int(x) % 10))
+#     x = int(x) / 10
+# if original == ''.join(mylist):
+#     print("True Palindrome")
+# else:
+#     print("False Not palindrome")
+
+# x = input()
+# for i in range(int(x),0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end="")
+#     print("")
+
+# x = input()
+# for i in range(1, int(x) + 1):
+#     for j in range(1, i + 1):
+#         print(i, end=" ")
+#     print('')
+
